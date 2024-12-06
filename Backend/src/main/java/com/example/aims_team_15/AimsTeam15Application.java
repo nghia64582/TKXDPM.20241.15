765775bd5c0c145a9da382678be8a4956c95b9ed
nghia64582 <nghia64582@gmail.com>
2024-12-06 23:14:47 +0700

    Minor bug

--- Backend/src/main/java/com/example/aims_team_15/AimsTeam15Application.java
+++ Backend/src/main/java/com/example/aims_team_15/AimsTeam15Application.java
@@ -2,8 +2,9 @@ package com.example.aims_team_15;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class AimsTeam15Application {
 
 	public static void main(String[] args) {
