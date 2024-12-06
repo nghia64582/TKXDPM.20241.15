e5f2f07d612600924b21a313b2af1ba5f60fd9f2
nghia64582 <nghia64582@gmail.com>
2024-12-06 23:08:00 +0700

    First commit

---
+++ Backend/src/main/java/com/example/aims_team_15/AimsTeam15Application.java
@@ -0,0 +1,13 @@
+package com.example.aims_team_15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AimsTeam15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AimsTeam15Application.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/example/aims_team_15/AimsTeam15ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aims_team_15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AimsTeam15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
